f45f19ae6ece3c4b2414dabebf2b9580b3cb0b25
Neerajan Dhakal <nirajandhakal634@gmail.com>
2025-09-18 20:14:00 +0545

    update detail form details tab

--- src/features/drawings/drawing-detail-tabs.tsx
+++ src/features/drawings/drawing-detail-tabs.tsx
@@ -1,7 +1,9 @@
 import MixedForm from '@/src/components/mixed-form'
 import { Drawing } from '@/src/constants/types'
+import { Ionicons } from '@expo/vector-icons'
+import { router } from 'expo-router'
 import React from 'react'
-import { Alert, Text, View } from 'react-native'
+import { Alert, Text, TouchableOpacity, View } from 'react-native'
 import { DrawingFormConfig, DrawingFormScehema } from './form-config'
 
 interface DrawingDetailTabsProps {
@@ -13,12 +15,36 @@ const DrawingDetailTabs: React.FC<DrawingDetailTabsProps> = ({ item, selectedTab
   switch (selectedTab) {
     case 'Details':
       return (
-        <MixedForm
-          formConfig={DrawingFormConfig}
-          onSubmit={() => Alert.alert('Submitted')}
-          initialValues={item}
-          formSchema={DrawingFormScehema}
-        />
+        <View className='flex-1'>
+          <View className='flex-row mb-2 justify-end'>
+            <TouchableOpacity
+              className='rounded-[5px] bg-gray-100 mr-2 p-2'
+              onPress={() => {
+                return Alert.alert('Are you sure you want to delete this item?', 'This action is irreversible.', [
+                  { text: 'Cancel', style: 'cancel' },
+                  { text: 'Delete', onPress: () => { } },
+                ]);
+              }}
+            >
+              <Ionicons name="trash" size={24} color="#FF3B30" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              className='rounded-[5px] bg-gray-100 mr-2 p-2'
+              onPress={() => router.push('/drawings')}
+            >
+              <Ionicons
+                name="close"
+                size={24}
+              />
+            </TouchableOpacity>
+          </View>
+          <MixedForm
+            formConfig={DrawingFormConfig}
+            onSubmit={() => Alert.alert('Submitted')}
+            initialValues={item}
+            formSchema={DrawingFormScehema}
+          />
+        </View>
       )
     default:
       return <View ><Text>Not Implemented</Text></View>
--- src/features/parts/part-detail-tabs.tsx
+++ src/features/parts/part-detail-tabs.tsx
@@ -1,8 +1,10 @@
 import MixedForm from '@/src/components/mixed-form'
 import { Part } from '@/src/constants/types'
 import React from 'react'
-import { Alert, Text, View } from 'react-native'
+import { Alert, Text, View, TouchableOpacity } from 'react-native'
 import { PartFormConfig, PartFormScehema } from './form-config'
+import { Ionicons } from '@expo/vector-icons'
+import { router } from 'expo-router'
 
 interface PartDetailTabsProps {
   item: Part;
@@ -13,12 +15,36 @@ const PartDetailTabs: React.FC<PartDetailTabsProps> = ({ item, selectedTab }) =>
   switch (selectedTab) {
     case 'Details':
       return (
-        <MixedForm
-          formConfig={PartFormConfig}
-          onSubmit={() => Alert.alert('Submitted')}
-          initialValues={item}
-          formSchema={PartFormScehema}
-        />
+        <View className='flex-1'>
+          <View className='flex-row mb-2 justify-end'>
+            <TouchableOpacity
+              className='rounded-[5px] bg-gray-100 mr-2 p-2'
+              onPress={() => {
+                return Alert.alert('Are you sure you want to delete this item?', 'This action is irreversible.', [
+                  { text: 'Cancel', style: 'cancel' },
+                  { text: 'Delete', onPress: () => { } },
+                ]);
+              }}
+            >
+              <Ionicons name="trash" size={24} color="#FF3B30" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              className='rounded-[5px] bg-gray-100 mr-2 p-2'
+              onPress={() => router.push('/drawings')}
+            >
+              <Ionicons
+                name="close"
+                size={24}
+              />
+            </TouchableOpacity>
+          </View>
+          <MixedForm
+            formConfig={PartFormConfig}
+            onSubmit={() => Alert.alert('Submitted')}
+            initialValues={item}
+            formSchema={PartFormScehema}
+          />
+        </View>
       )
     default:
       return <View ><Text>Not Implemented</Text></View>
